dcd0b7ede1fa950754b130ecaccc6c1f9929363d
Umar Farooq <umar.farooq@devigital.com>
2024-12-23 14:03:46 +0500

    dynamic manifest file

--- ghost/core/core/shared/settings-cache/index.js
+++ ghost/core/core/shared/settings-cache/index.js
@@ -1,6 +1,5 @@
 const CacheManager = require('./CacheManager');
 const publicSettings = require('./public');
-
 const cacheManager = new CacheManager({publicSettings});
 
 module.exports = cacheManager;
